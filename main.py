1b0920c71564f54adb2e714383a22fe38744997c
Juan Cruz <jc23729@gmail.com>
2020-12-10 15:16:25 +0000

    Going over functions

--- main.py
+++ main.py
@@ -3,4 +3,17 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
+#Simple function
 
+# def greet():
+#   print("Hello Angela")
+#   print("How do you do Jack Bauer?")
+#   print("Isn't the weather nice today?")
+# greet()
+
+#Functions that allow for input
+
+def greet_with_name(name):
+  print(f"Hello{name}")
+  print(f"How do you do {name}?")
+greet_with_name("Angela")
